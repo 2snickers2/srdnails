7f1b7f26897dfdaccdcfb495dccdb78dcb932fea
2snickers2 <93041229+2snickers2@users.noreply.github.com>
2026-06-22 20:10:57 +0300

    Add Russian language support with price and processing pages
    - Created `price.html` for displaying the price list in Russian.
    - Created `processing.html` to outline the tool sterilization process in Russian.
    - Added `main.css` for styling the new pages, including responsive design and layout adjustments.
    - Included a source map for CSS for easier debugging.

--- js/addPhoto.js
+++ js/addPhoto.js
@@ -3,20 +3,35 @@ const hiddenItems = document.querySelectorAll('.gallery-section__nails-foto.gall
 
 let isExpanded = false;
 
+const currentLink = window.location.href;
+let buttonOpenText
+let buttonCloseText
+
+if (currentLink.includes('/ru/')) {
+  buttonOpenText = 'Смотреть больше';
+  buttonCloseText = 'Свернуть';
+} else if (currentLink.includes('/en/')) {
+  buttonOpenText = 'View more';
+  buttonCloseText = 'Hide';
+} else {
+  buttonOpenText = 'Дивитись більше';
+  buttonCloseText = 'Згорнути';
+}
+
 button.addEventListener('click', () => {
   if (!isExpanded) {
     hiddenItems.forEach(item => {
       item.classList.remove('gallery-section__hidden');
       item.classList.add('shown');
     });
-    button.textContent = 'Згорнути';
+    button.textContent = buttonCloseText;
     isExpanded = true;
   } else {
     hiddenItems.forEach(item => {
       item.classList.remove('shown');
       item.classList.add('gallery-section__hidden');
     });
-    button.textContent = 'Дивитись більше ';
+    button.textContent = buttonOpenText;
     isExpanded = false;
   }
 });
--- js/iopenMenu.js
+++ js/iopenMenu.js
@@ -1,19 +1,20 @@
+const openBtn = document.getElementById('menuOpenBtn');
+const closeBtn = document.getElementById('menuCloseBtn');
+const menu = document.getElementById('menu');
+const menuLinks = document.querySelectorAll('.header__nav-link');
 
-  const openBtn = document.getElementById('menuOpenBtn');
-  const closeBtn = document.getElementById('menuCloseBtn');
-  const menu = document.getElementById('menu');
-  const menuLinks = document.querySelectorAll('.header__nav-link');
+openBtn.addEventListener('click', () => {
+  menu.classList.add('active');
+  document.querySelector('body').classList.add('lock-scroll');
+});
 
-
-  openBtn.addEventListener('click', () => {
-    menu.classList.add('active');
-  });
-
-  closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+  document.querySelector('body').classList.remove('lock-scroll');
+});
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
     menu.classList.remove('active');
+    document.querySelector('body').classList.remove('lock-scroll');
   });
-  menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      menu.classList.remove('active');
-    });
-  });
+});
